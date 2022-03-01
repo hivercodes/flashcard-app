c1c64737356f5c9ddaceec569388a84aa7371a12
Lars <lars.eriksson.mail@gmail.com>
2022-03-01 18:53:41 +0100

    Worked on making the titlecards rotate.

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 from tkinter import *
-
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+df = pandas.read_csv("data/french_words.csv")
+data_dict = df.to_dict(orient="records")
+
+first_run = 1
+
+def button_press():
+    pass
+
+
+
+def new_word():
+    global first_run
+    first_run = 0
+    new_index = random.randint(0, len(df)-1)
+    word_dict = data_dict[new_index]
+    canvas.itemconfig(word_text, text=word_dict["French"])
+
+
+
+
+def next_answer(word_dict):
+    canvas.itemconfig(word_text, text=word_dict["English"])
+    canvas.itemconfig(canvas_image, image=back_image)
+
+
+
+
+
 
 #window config
 window = Tk()
@@ -19,27 +48,24 @@ success_image = PhotoImage(file="images/right.png")
 
 #canvas config
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
-canvas.create_image(400, 263, image=front_image)
-language_text = canvas.create_text(400, 150, text="language", font=("Ariel", 40, "italic"))
+canvas_image = canvas.create_image(400, 263, image=front_image)
+language_text = canvas.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
 word_text = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 #buttons
-wrong_button = Button(image=fail_image, highlightthickness=0)
+wrong_button = Button(image=fail_image, highlightthickness=0, command=new_word)
 wrong_button.grid(column=0, row=1)
 
-success_button = Button(image=success_image, highlightthickness=0)
+success_button = Button(image=success_image, highlightthickness=0, command=new_word)
 success_button.grid(column=1, row=1)
 
 
 
 
 
-
-
-
-
-
+if first_run == 1:
+    new_word()
 
 
 
